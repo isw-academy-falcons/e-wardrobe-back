a570d03346eeea887ea4302f55c1e0f40b598a1a
olubunmee <bakreolubunmi@yahoo.com>
2023-09-22 09:40:08 +0100

    added plan, gender and email to login response

--- src/main/java/com/interswitchng/ewardrobe/dto/AuthenticationResponse.java
+++ src/main/java/com/interswitchng/ewardrobe/dto/AuthenticationResponse.java
@@ -9,8 +9,11 @@ public class AuthenticationResponse {
     private String accessToken;
     private String userId;
     private String fullName;
+    private String plan;
+    private String gender;
+    private String email;
 
-    public static AuthenticationResponse of(String jwtToken, String userId, String fullName){
-        return new AuthenticationResponse(jwtToken, userId, fullName);
+    public static AuthenticationResponse of(String jwtToken, String userId, String fullName, String plan, String gender, String email){
+        return new AuthenticationResponse(jwtToken, userId, fullName, plan, gender, email);
     }
 }
